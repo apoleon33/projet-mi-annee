ae871524aa7de523f116db37383d6e576698f512
ghighi451 <ghislain.cattus@student.junia.com>
2025-01-27 16:52:27 +0100

    update 02

--- read.py
+++ read.py
@@ -5,28 +5,30 @@ tree = ET.parse('diagramme-test.xml')
 root = tree.getroot()
 
 class Cells:
-    name = 0
+    Title = 0
     ID = 0
     def __init__(self):
         self.Target = []  # list member variable, unique to each instantiated class
-    
+
+
+
 liste_class = []
 
 
 for child in root[0]:                   #positionement dans le fichié xml a la 3eme balise
 
         if 'name' in child.attrib : 
-           # print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
+            print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
 
             Cell = Cells()
-            Cell.name = child.attrib.get('name')
+            Cell.Title = child.attrib.get('name')
             Cell.ID = child.attrib.get('id')
             liste_class.append(Cell)
         
         
             
         if 'sourceRef' in child.attrib:
-            #print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
+            print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
             
             for i in range(len(liste_class)):
                 if liste_class[i].ID == child.attrib.get('sourceRef'):
@@ -34,10 +36,4 @@ for child in root[0]:                   #positionement dans le fichié xml a la 
 
             
             
-       #print(child.tag, child.attrib)        #lecture de toute la ligne
-        
-
-        
-        
-        
-        
+        print(child.tag, child.attrib)        #lecture de toute la ligne
