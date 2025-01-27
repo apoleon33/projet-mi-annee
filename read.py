b4a692a898c0df01e8d88863d5fe7420bb03818a
ghighi451 <ghislain.cattus@student.junia.com>
2025-01-27 16:10:03 +0100

    update 01 - fixed list updated in class

--- read.py
+++ read.py
@@ -4,10 +4,11 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('diagramme-test.xml')
 root = tree.getroot()
 
-class Nom_Prenom:
-    Nom_Prenom = 0
+class Cells:
+    name = 0
     ID = 0
-    Target = []
+    def __init__(self):
+        self.Target = []  # list member variable, unique to each instantiated class
     
 liste_class = []
 
@@ -15,27 +16,26 @@ liste_class = []
 for child in root[0]:                   #positionement dans le fichié xml a la 3eme balise
 
         if 'name' in child.attrib : 
-            print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
-            
-            if child.attrib.get('name') == 'Nom Prenom':
-                NP = Nom_Prenom()
-                NP.Nom_Prenom = child.attrib.get('name')
-                NP.ID = child.attrib.get('id')
-                liste_class.append(NP)
+           # print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
+
+            Cell = Cells()
+            Cell.name = child.attrib.get('name')
+            Cell.ID = child.attrib.get('id')
+            liste_class.append(Cell)
         
         
             
         if 'sourceRef' in child.attrib:
-            print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
+            #print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
             
             for i in range(len(liste_class)):
                 if liste_class[i].ID == child.attrib.get('sourceRef'):
                     liste_class[i].Target.append(child.attrib.get('targetRef'))
+
             
             
-        print(child.tag, child.attrib)        #lecture de toute la ligne
+       #print(child.tag, child.attrib)        #lecture de toute la ligne
         
-print(liste_class)
 
         
         
