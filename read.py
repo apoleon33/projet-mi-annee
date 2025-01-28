31e8c13e26635be7d77cfcd2ad26ca85ce3e70ea
ghighi451 <ghislain.cattus@student.junia.com>
2025-01-28 09:11:36 +0100

    update3: can now use a Type and a Content for each cell with this setup Type: Content

--- read.py
+++ read.py
@@ -1,34 +1,42 @@
 import os
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('diagramme-test.xml')
-root = tree.getroot()
+
 
 class Cells:
-    Title = 0
+    Type = 0
+    Content = 0
     ID = 0
     def __init__(self):
         self.Target = []  # list member variable, unique to each instantiated class
 
 
 
-liste_class = []
+
+def readXML(data):
+    tree = ET.parse(data)
+    root = tree.getroot()
+    liste_class = []
 
 
-for child in root[0]:                   #positionement dans le fichié xml a la 3eme balise
+    for child in root[0]:                   #positionement dans le fichié xml a la 3eme balise
 
         if 'name' in child.attrib : 
-            print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
+            #print(child.attrib.get('name'), " | ", child.attrib.get("id"))      #lecture d'une valeur specifique
 
-            Cell = Cells()                                                      #creation des cellules
-            Cell.Title = child.attrib.get('name')
+            Cell = Cells()       #creation des cellules
+
+            cell_title = read_title(child.attrib.get('name'))            #call func read_title to split the variables
+            Cell.Type = cell_title[0]
+            Cell.Content = cell_title[1]
+
             Cell.ID = child.attrib.get('id')
             liste_class.append(Cell)
         
         
             
         if 'sourceRef' in child.attrib:
-            print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
+            #print("Source fleche : ", child.attrib.get('sourceRef'), "| Target Fleche: ", child.attrib.get('targetRef'))
             
             for i in range(len(liste_class)):                              #boucle de comparaison pour trié les fleche
                 if liste_class[i].ID == child.attrib.get('sourceRef'):
@@ -36,4 +44,15 @@ for child in root[0]:                   #positionement dans le fichié xml a la 
 
             
             
-        print(child.tag, child.attrib)        #lecture de toute la ligne
+        #print(child.tag, child.attrib)        #lecture de toute la ligne
+    return(liste_class)
+
+
+def read_title(data):          #read Cells datas
+    data = data.split(":")     #split the data
+    type = data[0].strip()     #.split() delete the space at the start/end if the str
+    content = data[1].strip()
+
+    return([type,content])
+
+data = readXML('diagramme-test(1).xml')
