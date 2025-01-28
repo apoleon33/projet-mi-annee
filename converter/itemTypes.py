6edd11e9f4bb1368f53bd38bb1226083cde8f70b
Simon Dumesny <simondumesny@gmail.com>
2025-01-28 10:36:03 +0000

    updated enum

--- converter/itemTypes.py
+++ converter/itemTypes.py
@@ -5,4 +5,10 @@ class ItemTypes(Enum):
     """
     Tous les types de cases que l'utilisateur peut créer
     """
-    NP = "NP"
+    NP = "NP"  # Nom/prénom
+    date = "date"  # jj / mm / aaaa
+    titre = "Titre"  # titre
+    lieu = "lieu"  # place
+    dsc = "dsc"  # description
+    freq = "Frequ"  # Fréquence
+    IMP = "IMP"  # importance(niveau 1 - 2 - 3 - 4)
